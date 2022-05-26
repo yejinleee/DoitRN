84bc8eac8ce99a24790efe6ab6f35781d647a8b1
yejinleee <yejin9487@gmail.com>
2022-05-27 02:55:38 +0900

    Docs

--- ch04/ch04_3/src/screens/PeresonUsingValueState.tsx
+++ ch04/ch04_3/src/screens/PeresonUsingValueState.tsx
@@ -1,4 +1,4 @@
-import React, {FC} from 'react';
+import React, {FC, useCallback, useState} from 'react';
 import {Text, View, Image, Alert} from 'react-native';
 import * as D from '../data';
 import {styles} from './Person.style';
@@ -13,39 +13,45 @@ export type PersonProps = {
   person: D.IPerson;
 };
 
-const avatarPressed = () => Alert.alert('avatar pressed');
-const deletePressed = () => Alert.alert('delete pressed');
-const countIconPressed = (name: string) => () =>
-  Alert.alert(`${name} pressed.`);
-
 // prettier-ignore
-const Person: FC<PersonProps> = ({person}) => {
+const Person: FC<PersonProps> = ({person: initialPerson}) => { //initialPerson타입이 어디서온걸까뭘까
+  const avatarPressed = useCallback(() => Alert.alert('avatar pressed'),[]);
+  const deletePressed = useCallback(() => Alert.alert('delete pressed'),[]);
+
+  const [comment, setComment] = useState<number>(0);
+  const [retweet, setRetweet] = useState<number>(0);
+  const [heart, setHeart] = useState<number>(0);
+
+  const commentPressed = useCallback(()=> {setComment(comment => comment + 1);}, []);
+  const retweetPressed = useCallback(()=> {setRetweet(retweet => retweet + 1);}, []);
+  const heartPressed = useCallback(()=> {setHeart(heart => heart + 1);}, []);
+
   return (
     <View style={[styles.view]}>
       <View style={[styles.leftView]}>
-        <Avatar imageStyle={[styles.avatar]} uri={person.avatar} size={50} onPress={avatarPressed} />
+        <Avatar imageStyle={[styles.avatar]} uri={initialPerson.avatar} size={50} onPress={avatarPressed} />
       </View>
       <View style={[styles.rightView]}>
-        <Text style={[styles.name]}>{person.name}</Text>
-        <Text style={[styles.email]}>{person.email}</Text>
+        <Text style={[styles.name]}>{initialPerson.name}</Text>
+        <Text style={[styles.email]}>{initialPerson.email}</Text>
         <View style={[styles.dateView]}>
           <Text style={[styles.text]}>
-            {moment(person.createdDate).startOf('day').fromNow()}
+            {moment(initialPerson.createdDate).startOf('day').fromNow()}
           </Text>
           <Icon name="trash-can-outline" size={26} color={Colors.lightBlue500} onPress={deletePressed} />
         </View>
-        <Text numberOfLines={3} ellipsizeMode="tail" style={[styles.text, styles.comments]}>{person.comments}</Text>
-        <Image style={[styles.image]} source={{uri: person.image}} />
+        <Text numberOfLines={3} ellipsizeMode="tail" style={[styles.text, styles.comments]}>{initialPerson.comments}</Text>
+        <Image style={[styles.image]} source={{uri: initialPerson.image}} />
         <View style={[styles.nameEmailView]}>
-          <IconText viewStyle={[styles.touchableIcon]} onPress={countIconPressed('comment')}
+          <IconText viewStyle={[styles.touchableIcon]} onPress={commentPressed}
            name="twitter" size={24} color={Colors.purple500}
-           textStyle={[styles.iconText]} text={person.counts.comment} />
-          <IconText viewStyle={[styles.touchableIcon]} onPress={countIconPressed('retweet')}
+           textStyle={[styles.iconText]} text={comment} />
+          <IconText viewStyle={[styles.touchableIcon]} onPress={retweetPressed}
            name="twitter" size={24} color={Colors.purple500}
-           textStyle={[styles.iconText]} text={person.counts.retweet} />
-          <IconText viewStyle={[styles.touchableIcon]} onPress={countIconPressed('heart')}
+           textStyle={[styles.iconText]} text={retweet} />
+          <IconText viewStyle={[styles.touchableIcon]} onPress={heartPressed}
            name="twitter" size={24} color={Colors.purple500}
-           textStyle={[styles.iconText]} text={person.counts.heart} />
+           textStyle={[styles.iconText]} text={heart} />
         </View>
       </View>
     </View>
